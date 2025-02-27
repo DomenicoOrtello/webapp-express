9bb3a967a65bb918d3ade652b08c38de13bf1a61
Ortello7 <domenicoortello00@gmail.com>
2025-02-27 01:47:26 +0100

    added routes to router file

--- routers/movieRouters.js
+++ routers/movieRouters.js
@@ -5,4 +5,10 @@ const movieControllers = require ("../controller/movieController")
 // INDEX
 router.get("/", movieControllers.index)
 
+// SHOW
+router.get("/", movieControllers.show)
+
+// DESTROY
+router.delete("/", movieControllers.destroy)
+
 module.exports = router;
