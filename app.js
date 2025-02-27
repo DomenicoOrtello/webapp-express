d45c16b6850f4596ca7211ea09bf4df9db55e02d
Ortello7 <domenicoortello00@gmail.com>
2025-02-27 01:45:46 +0100

    feat .env, middleware, controller, data, router

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const express = require('express')
-const app = express()
-const cors = require("cors")
-const { PORT, FE_URL} = process.env
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const movieRouter = require ("./routers/movieRouters")
+const errorsHandler = require("./middleware/errorsHandler");
+const notFound = require ("./middleware/notFound")
+const { PORT, FE_URL } = process.env;
 
 // Middleware dei file statici
 app.use(express.static("public"));
 
+// Middleware del parsing del req.body
+app.use(express.json());
+
 // Middleware CORS
 app.use(
-    cors({
-      origin: FE_URL,
-    })
-  );
+  cors({
+    origin: FE_URL,
+  })
+);
+
+// Rotte dell'app
+app.use("/movie", movieRouter)
+
+// Middlewares per la gestione degli errori 404 e 500
+app.use(errorsHandler);
+app.use(notFound);
+
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
---
+++ routers/movieRouters.js
@@ -0,0 +1,8 @@
+const express = require ("express")
+const router = express.Router()
+const movieControllers = require ("../controller/movieController")
+
+// INDEX
+router.get("/", movieControllers.index)
+
+module.exports = router;
